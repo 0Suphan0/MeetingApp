0424d7a50d1dd6ae826c2b8b60882a323b1b7a79
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2024-02-20 10:54:40 +0300

    Viewbag ile title kısımları dinamik hale getirildi.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,8 +4,13 @@ namespace MeetingApp.Controllers
 {
     public class HomeController : Controller
     {
+        
         public IActionResult Index()
         {
+            int saat = DateTime.Now.Hour;
+            string title=saat < 12 ? "Günaydın" : "İyi Günler";
+            ViewBag.header = title;
+            ViewBag.User = "Süphan";
             return View();
         }
     }
